e35b13aae29e8040387120e43b45d9d5ab76263f
Timothy Gebhard <mail@timothygebhard.de>
2019-08-26 16:05:17 +0100

    Add setup.py and basic package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='AutoPower',
+      version='0.0',
+      description='AutoPower',
+      url='https://github.com/florpi/AutoPower',
+      install_requires=['camb',
+                        'h5py',
+                        'jupyter',
+                        'matplotlib',
+                        'numpy',
+                        'scikit-learn',
+                        'scipy',
+                        'seaborn',
+                        'torch',
+                        'tqdm'],
+      packages=['autopower'],
+      zip_safe=False)
