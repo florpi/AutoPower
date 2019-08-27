c04f3b15f4da597766de267ddea962a9289689a9
carol <florpisoteadadejardin@gmail.com>
2019-08-27 09:15:44 +0100

    changed to ratio

--- data/generate_power_spectrum.py
+++ data/generate_power_spectrum.py
@@ -3,7 +3,7 @@ from camb import model, initialpower
 
 
 def get_power_spectrum(h, omc, omb = 0.0486,
-     redshift = 0., nkpoints = 200):
+     redshift = 0., nkpoints = 200, mode = 'non-linear'):
     
     H0 = 100. * h
     ombh2 = omb * h **2
@@ -14,13 +14,19 @@ def get_power_spectrum(h, omc, omb = 0.0486,
     pars.InitPower.set_params()
     
     pars.set_matter_power(redshifts=[redshift], kmax=2.0)
+
+    if mode == 'linear':
+
+        pars.NonLinear = model.NonLinear_none
     
-    #Non-Linear spectra (Halofit)
-    pars.NonLinear = model.NonLinear_both
+    elif mode == 'non-linear':
+        #Non-Linear spectra (Halofit)
+        pars.NonLinear = model.NonLinear_both
+
     results = camb.get_results(pars)
     results.calc_power_spectra(pars)
-    kh_nonlin, z_nonlin, pk_nonlin = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1,
-                                                                       npoints = nkpoints)
+    k, z, pk = results.get_matter_power_spectrum(minkh=1e-4, maxkh=1,
+                                                        npoints = nkpoints)
     
-    return kh_nonlin, pk_nonlin[0, :]
+    return k, pk[0, :] # 0 for only one redshift
   
